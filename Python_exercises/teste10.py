2cfc1e7dc8435749882fb44e110685bac0010fe8
wevertonbezzera013 <wevertonbezzera013@gmail.com>
2020-10-04 16:03:03 -0300

    Create teste10.py

---
+++ Python_exercises/teste10.py
@@ -0,0 +1,3 @@
+n1 = int(input('\033[7;34;41m Digite um valor: \033[m'))
+n2 = int(input('\033[7;34;41m Digite um valor: \033[m'))
+print('\033[7;33;40m Você digitou os valores \033[35m{}\033[m e \033[7;35m{}\033[m!!! \033[m'.format(n1, n2))
