14a80f0ca3b9eaf5b7b87b7486838a5553ab9d49
wevertonbezzera013 <wevertonbezzera013@gmail.com>
2020-10-04 15:58:58 -0300

    Create desafio32.py

---
+++ Python_exercises/desafio32.py
@@ -0,0 +1,9 @@
+if ano == 0:
+from datetime import date
+ano = int(input('Digite um ano: '))
+if ano == 0:
+    ano = date.today().year()
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano é bissexto!')
+else:
+    print('O ano não é bissexto!')
