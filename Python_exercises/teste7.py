bc81595ac0ccdf1e94c86d4e8e9585b7da1ab446
wevertonbezzera013 <wevertonbezzera013@gmail.com>
2020-10-04 16:03:57 -0300

    Create teste7.py

---
+++ Python_exercises/teste7.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite seu nome: ')).strip()
+if nome == 'Weverton':
+    print('Olá Weverton gatão!')
+else:
+    print('Saia! Saia do grupo agora!')
+print('Você não é aceito(a) neste grupo {}!!!'.format(nome))
