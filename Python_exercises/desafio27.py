2fb9ef55e57085176ac5af6bd77ab95e3076373d
wevertonbezzera013 <wevertonbezzera013@gmail.com>
2020-10-04 15:58:44 -0300

    Create desafio27.py

---
+++ Python_exercises/desafio27.py
@@ -0,0 +1,5 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+n = nome.split()
+print('Prazer em te conhecer!')
+print('Seu primeiro nome é {}.'.format(n[0]))
+print('Seu último nome é {}.'.format([n.len[n] - 1]))
