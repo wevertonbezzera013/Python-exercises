e438d12649bf329adebce5e287d1f97b312f75ca
wevertonbezzera013 <wevertonbezzera013@gmail.com>
2020-10-04 15:58:28 -0300

    Create desafio22.py

---
+++ Python_exercises/desafio22.py
@@ -0,0 +1,8 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Analisando seu nome...')
+print('Seu nome em letras maiúsculas é {}.'.format(nome.upper()))
+print('Seu nome em letras minúsculas é {}.'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras.'.format(len(nome)-nome.count(' ')))
+#print('Seu primeiro nome {} tem {} letras.'.format(nome, nome.find(' ')))
+separa = nome.split()
+print('Seu primeiro nome {} tem {} letras.'.format(nome, len(separa[0])))
