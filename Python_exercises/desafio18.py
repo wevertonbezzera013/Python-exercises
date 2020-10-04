5d1af3877291c764a9634dc5e23b0965a62438e9
wevertonbezzera013 <wevertonbezzera013@gmail.com>
2020-10-04 15:58:21 -0300

    Create desafio18.py

---
+++ Python_exercises/desafio18.py
@@ -0,0 +1,5 @@
+import math
+ang = float(input('Digite o ângulo: '))
+print('O SENO do ângulo é {:.2f}'.format(math.sin(math.radians(ang))))
+print('O COSSENO do ângulo é {:.2f}'.format(math.cos(math.radians(ang))))
+print('A TANGENTE do ângulo é {:.2f}'.format(math.tan(math.radians(ang))))
