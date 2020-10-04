44a13dd3fe31ff7e6ff62bba3667cec6dc4d0648
wevertonbezzera013 <wevertonbezzera013@gmail.com>
2020-10-04 16:03:54 -0300

    Create teste6.py

---
+++ Python_exercises/teste6.py
@@ -0,0 +1,25 @@
+frase = '   Curso em vídeo Python   '
+#print(frase[3])
+#print(frase[3:12])
+#print(frase[:12])
+#print(frase[12:])
+#print(frase[1:15:2])
+
+#print("""Eu sou um programador
+#Eu gosto de programar programas
+#Programação é incrível demais
+#Eu estou escrevendo um texto.""")
+
+#print(frase.upper().count('o'))
+#print(len(frase.strip()))
+#print(frase.replace('vídeo', 'patrocínio'))
+#frase.replace('vídeo', 'patrocínio')
+#frase = frase.replace('vídeo', 'patrocínio')
+#print(frase)
+#print('Curso' in frase)
+#print(frase.lower().find('vídeo'))
+#print(frase.split())
+
+#dividido = frase.split()
+#print(dividido[0])
+#print(dividido[2] [3])
