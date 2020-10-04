9db7513f5888d28ceabed0136ceaae2c15b16996
wevertonbezzera013 <wevertonbezzera013@gmail.com>
2020-10-04 15:53:49 -0300

    Create desafio11.py

---
+++ Python_exercises/desafio11.py
@@ -0,0 +1,5 @@
+n1 = float(input('Altura: '))
+n2 = float(input('Largura: '))
+A = n1 * n2
+t = (n1 * n2)/2
+print('A altura da parede é {}, a largura é {}, sua área é {} e serão usados {} baldes de tinta.'.format(n1, n2, A, t))
